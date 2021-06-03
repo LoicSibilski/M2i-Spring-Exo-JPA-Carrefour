5db44af6a6b092046ffcdcc2fbf72d0b72718dfa
Loic Sibilski <aeneas62@hotmail.fr>
2021-06-03 15:10:55 +0200

    envoie des données initial pour le projet AKA (=Also Known As (= Connue aussi pour ) ) 'initial commit'

---
+++ src/main/java/com/m2i/exo/M2ISpringExoJpaCarroufApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.exo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2ISpringExoJpaCarroufApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2ISpringExoJpaCarroufApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2i/exo/M2ISpringExoJpaCarroufApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.exo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ISpringExoJpaCarroufApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
